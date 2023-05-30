db73e0edbef15e89c1e0a1d0655a1afc8c7cf354
Jankovv295 <jankov295@gmail.com>
2023-05-31 01:24:57 +0200

    Homework

---
+++ 3. FK, Join/SQLQuery6.sql
@@ -0,0 +1,31 @@
+﻿--○ Find all Students with FirstName = Antonio
+select *
+from Student
+where FirstName = 'Antonio'
+
+--○ Find all Students with DateOfBirth greater than ‘01.01.1999’
+select *
+from Student
+where DateOfBirth > '1999.01.01'
+
+--○ Find all Students with LastName starting With ‘J’ enrolled in January/1998
+select *
+from Student
+where LastName like 'J%' and EnrolledDate like '1998.01'
+
+--○ List all Students ordered by FirstName
+select * 
+from Student
+order by FirstName
+
+--○ List all Teacher Last Names and Student Last Names in single result set.
+--Remove duplicates
+select *
+from Teacher
+
+
+
+--○ Create Foreign key constraints from diagram or with script
+--○ List all possible combinations of Courses names and AchievementType
+--names that can be passed by student
+--○ List all Teachers without exam Grade
---
+++ 5. Built in and Scalar functions/SQLQuery7.sql
@@ -0,0 +1,51 @@
+﻿--● Declare scalar variable for storing FirstName values
+--• Assign value ‘Antonio’ to the FirstName variable
+--• Find all Students having FirstName same as the variable
+DECLARE @FirstName nvarchar(50)
+SET @FirstName = 'Antonio'
+
+SELECT *
+FROM Student
+WHERE FirstName = @FirstName
+
+--● Declare table variable that will contain StudentId, StudentName and DateOfBirth
+--• Fill the table variable with all Female students
+DECLARE @VarTableFemaleStudents TABLE (
+StudentId int,
+StudentName nvarchar(50),
+DateOfBirth datetime
+)
+
+INSERT INTO @VarTableFemaleStudents(StudentId, StudentName, DateOfBirth)
+SELECT S.ID, S.FirstName, DateOfBirth
+FROM Student S
+WHERE Gender = 'F'
+GROUP BY S.ID, S.FirstName, DateOfBirth
+
+SELECT * FROM @VarTableFemaleStudents
+
+--● Declare temp table that will contain LastName and EnrolledDate columns
+--• Fill the temp table with all Male students having First Name starting with ‘A’
+--• Retrieve the students from the table which last name is with 7 characters
+CREATE TABLE #TempTable(LastName nvarchar(50), EnrolledDate datetime)
+
+INSERT INTO #TempTable(LastName, EnrolledDate)
+SELECT S.LastName, S.EnrolledDate
+FROM Student S
+WHERE Gender = 'M' AND FirstName like 'A%'
+GROUP BY S.LastName, S.EnrolledDate
+
+SELECT * FROM #TempTable
+
+SELECT *
+FROM #TempTable t
+WHERE LEN(t.LastName) = 7;
+
+--● Find all teachers whose FirstName length is less than 5 and
+--• the first 3 characters of their FirstName and LastName are the same
+
+SELECT *
+FROM Teacher T
+WHERE LEN(T.FirstName) < 5 AND LEFT(T.FirstName, 3) = LEFT(T.LastName, 3)
+
+
